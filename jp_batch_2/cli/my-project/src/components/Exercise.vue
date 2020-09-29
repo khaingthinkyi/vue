c5eb062fa526f12bc3fbfd716655f8ff43c73916
Khaing Thin Kyi <khaingthinkyi2k20@gmail.com>
2020-09-29 15:27:36 +0630

    vue with cli

---
+++ jp_batch_2/cli/my-project/src/components/Exercise.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1 class="text-danger">This is an Exercise Component!</h1>
+	</div>
+</template>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css">
+	
+</style>
